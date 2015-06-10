818defc3dec1d8e5123a947748d7c8588d06a118
Shaan Shah <shahshaan@gmail.com>
2015-06-10 15:57:53 -0700

    [feat] adds basic styling to the tracker page with calendar and one message

--- src/Tracker.jsx
+++ src/Tracker.jsx
@@ -15,14 +15,28 @@ var Tracker = React.createClass({
   handleChange: function(dateObject) {
     console.log('onChange is calling this');
     var dateClicked = (dateObject.getMonth() + 1).toString() + '/' + dateObject.getDate().toString() + '/' + dateObject.getFullYear().toString();
+    // var dateClicked = dateObject.toString;
     this.setState({dateClicked: dateClicked});
   },
   render: function() {
     var dateClicked = this.state.dateClicked;
     return (
-      <div>
-        <Calendar defaultValue={new Date()} footer={true} onChange={this.handleChange} />
-        <div>Date selected: {dateClicked}</div>
+      <div class="row" id="tracker">
+        <h1>Shopping Tracker</h1>
+        <div className="col-md-6">
+          <Calendar defaultValue={new Date()} footer={true} onChange={this.handleChange} />
+        </div>
+        <div className="col-md-6 single-day">
+          <div><h2>{dateClicked}</h2></div>
+          <div className="logged-message">
+            <div className="logged-message-title">
+              Chris #shopped
+            </div>
+            <div className="logged-message-text">
+              Bought laundry detergent, pizza, Curry jersey, and some shampoo for my beard #shopped
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
