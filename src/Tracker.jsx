b43f2d213e607224f8b9b5bfe985f31c0f87ab8d
Chris Grovers <chrisgrovers@gmail.com>
2015-06-11 19:30:41 -0700

    [feat] tracker now saves 'events' and 'dateClicked' to window, so that Calendars DayComponent can access these values

--- src/Tracker.jsx
+++ src/Tracker.jsx
@@ -3,14 +3,73 @@ var Eventful = require('eventful-react');
 var CalendarComponent = require('react-widgets').Calendar
 var Calendar = require('./Calendar.jsx');
 
-
-var Tracker = React.createClass({
+var Tracker = Eventful.createClass({
 
   getInitialState: function() {
     var todaysDateInstance = new Date();
     var todaysDate = (todaysDateInstance.getMonth() + 1).toString() + '/' + todaysDateInstance.getDate().toString() + '/' + todaysDateInstance.getFullYear().toString();
+    var events = {
+        "6/07/15":[{
+            1:{
+              id:1,
+              name:'person1',
+              sender_id:11,
+              sender_type:'person',
+              text:'testing 1',
+              user_id:11
+            }
+          }, {
+            2:{
+              id:2,
+              name:'person2',
+              sender_id:12,
+              sender_type:'person',
+              text:'testing 2',
+              user_id:12
+            }
+          }],
+        "6/10/15": [{
+            3:{
+              id:3,
+              name:'person3',
+              sender_id:13,
+              sender_type:'person',
+              text:'testing 3',
+              user_id:13
+            }
+          }, {
+            4:{
+              id:4,
+              name:'person4',
+              sender_id:14,
+              sender_type:'person',
+              text:'testing 4',
+              user_id:14
+            }
+          }],
+        "6/11/15":[{
+            5:{
+              id:5,
+              name:'person5',
+              sender_id:15,
+              sender_type:'person',
+              text:'testing 5',
+              user_id:15
+            }
+          }, {
+            6:{
+              id:6,
+              name:'person6',
+              sender_id:16,
+              sender_type:'person',
+              text:'testing 6',
+              user_id:16
+            }
+          }]
+      }
+    window.events = events;
     return {
-      dateClicked: todaysDate,
+      dateClicked: todaysDate
     };
   },
   componentDidMount: function() {
@@ -23,6 +82,8 @@ var Tracker = React.createClass({
     });
   },
   render: function() {
+      window.dateClicked = this.state.dateClicked;
+
     return (
       <div class="row" id="tracker">
         <h1>Shopping Tracker</h1>
@@ -46,3 +107,5 @@ var Tracker = React.createClass({
 });
 
 module.exports = Tracker;
+
+
